26def8741d150f01b06647ca5b12d8bf8810b961
Keven <kevendicamargo@gmail.com>
2025-04-24 20:43:59 -0300

    chore: removendo Speed Insights
    por não ser compativel com o React puro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import './styles/index.sass';
 import App from './App';
 
@@ -10,6 +9,5 @@ root.render(
   <React.StrictMode>
     <App />
     <Analytics />
-    <SpeedInsights />
   </React.StrictMode>
 );
