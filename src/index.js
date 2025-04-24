c2734b5a812be1debad99f4d92f028e600560708
Keven <kevendicamargo@gmail.com>
2025-04-24 16:57:20 -0300

    chore: adicionar Speed Insights da Vercel para coleta de métricas de desempenho

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './styles/index.sass';
 import App from './App';
 
@@ -9,5 +10,6 @@ root.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
